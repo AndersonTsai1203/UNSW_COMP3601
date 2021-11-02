2ec5f3f9076a7dbac26bb7916289e30567c7c842
AndersonTsai1203 <tsai.anderson1203@gmail.com>
2021-11-02 23:21:30 +1100

    Updated code

--- uniform_random_number_generator/uniform_random_GEN.vhd
+++ uniform_random_number_generator/uniform_random_GEN.vhd
@@ -11,21 +11,20 @@ end uniform_random_GEN;
 
 architecture Behavioral of uniform_random_GEN is
 
-signal lfsr_reg         : std_logic_vector (15 downto 0);
+signal lfsr_reg         : std_logic_vector (15 downto 0) := prime;
 signal feedback         : std_logic := '0';
-
+signal temp_reg         : integer;
 begin
-    feedback <= lfsr_reg(15) xor lfsr_reg(14) xor lfsr_reg(12) xor lfsr_reg(3);
+    
     process(clk, rst) is
-        variable temp_reg : integer;
     begin
         if (rst = '1') then
-            lfsr_reg <= prime;
+            lfsr_reg <= (0 => '1', others => '0');
         elsif rising_edge(clk) then
+            feedback <= lfsr_reg(15) xor lfsr_reg(14) xor lfsr_reg(12) xor lfsr_reg(3);
             lfsr_reg <= feedback & lfsr_reg(15 downto 1);
-            temp_reg := conv_integer(unsigned(lfsr_reg)) mod conv_integer(unsigned(prime));
+            temp_reg <= conv_integer(unsigned(lfsr_reg)) mod conv_integer(unsigned(prime));
             result <= conv_std_logic_vector (temp_reg, result'length);         
         end if;
     end process;
 end Behavioral;
-
